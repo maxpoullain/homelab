e49b85483a25db897a1d87d3091cd6b2dc8816c1
Max <poullain.maxime@gmail.com>
2023-12-19 19:32:49 +0100

    [tools][oc] update tmp script

--- tools/scripts/oc/pi-tmp.sh
+++ tools/scripts/oc/pi-tmp.sh
@@ -6,5 +6,5 @@
 cpu=$(</sys/class/thermal/thermal_zone0/temp)
 echo "$(date) @ $(hostname)"
 echo "-------------------------------------------"
-echo "GPU => $(/opt/vc/bin/vcgencmd measure_temp)"
+echo "GPU => $(vcgencmd measure_temp | sed -e 's/\<temp\>//g' | sed 's/\=//g')"
 echo "CPU => $((cpu/1000))'C"
