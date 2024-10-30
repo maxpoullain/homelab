af02a28c17c775db558a89c54fd8859722c2f476
Max <poullain.maxime@gmail.com>
2024-10-30 19:28:06 +0100

    [tools] update tooling

--- tools/scripts/oc/pi-monito.sh
+++ tools/scripts/oc/pi-monito.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Script: my-pi-temp.sh
-# Purpose: Display the ARM CPU and GPU  temperature of Raspberry Pi 2/3 
+# Purpose: Display the ARM CPU and GPU  temperature of Raspberry Pi 2/3
 # Author: Vivek Gite <www.cyberciti.biz> under GPL v2.x+
 # -------------------------------------------------------
 cpu=$(</sys/class/thermal/thermal_zone0/temp)
 echo "$(date) @ $(hostname)"
 echo "-------------------------------------------"
-echo "GPU => $(vcgencmd measure_temp | sed -e 's/\<temp\>//g' | sed 's/\=//g')"
-echo "CPU => $((cpu/1000))'C"
+echo "CPU Freq => $(vcgencmd measure_clock arm | sed -e 's/frequency(48)=//g')"
+echo "CPU Temp => $((cpu/1000))'C"
+echo "GPU Temp => $(vcgencmd measure_temp | sed -e 's/temp=//g')"
